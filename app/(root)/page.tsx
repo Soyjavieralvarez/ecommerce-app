e7894fcdb7ad4bd211d72e001e71d6796f5cd398
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-08-24 19:07:34 +0200

    sign in & sign up done!

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetUpPage = () => {
     return (
       <div className="p-4">
-        <Button size="default" variant={"outline"}>Clic me</Button>
+       <UserButton afterSignOutUrl="/"/>
       </div>
     )
   }
+
+  export default SetUpPage;
