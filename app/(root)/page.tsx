ecdd27b3f7fd28bb0f4859d382a55b79cc5b10f3
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-08-25 18:24:08 +0200

    added hooks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
+import { Children } from 'react';
 
 const SetUpPage = () => {
     return (
       <div className="p-4">
-       <UserButton afterSignOutUrl="/"/>
+       <Modal title="Test" description="Test description" isOpen onClose={() => {}}>
+        Children
+       </Modal>
       </div>
     )
   }
 
   export default SetUpPage;
+
+  
---
+++ hooks/use-store-modal.tsx
@@ -0,0 +1,8 @@
+import { create } from "zustand"
+
+interface useStoreModalStore {
+    isOpen: boolean;
+    onOpen: () => {};
+    onClose: () => {};
+}
+
