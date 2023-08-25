35b197f50fd172aae7240de16065b21918b5a882
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-08-25 20:01:55 +0200

    started form from react!

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import * as z from "zod";
 import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from '@/hooks/use-store-modal';
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object ({
+    name: z.string().min(1),
+});
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+
+    const from = useForm
 
     return (
         <Modal
