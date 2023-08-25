257efed620a875070962da6f98abbc912af50f47
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-08-25 18:38:58 +0200

    modal created

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Modal } from "../ui/modal";
+import { useStoreModal } from '../../hooks/use-store-modal';
+
+export const StoreModal = () => {
+    const storeModal = useStoreModal();
+
+    return (
+    <Modal
+    title="Create store"
+    description="Add a new store to manage products and categories"
+    isOpen={false}
+    onClose={() => {}}
+    >
+       
+
+        Future Create Store Form
+    </Modal>
+    )
+}
--- hooks/use-store-modal.tsx
+++ hooks/use-store-modal.tsx
@@ -2,7 +2,14 @@ import { create } from "zustand"
 
 interface useStoreModalStore {
     isOpen: boolean;
-    onOpen: () => {};
-    onClose: () => {};
-}
+    onOpen: () => void;
+    onClose: () => void;
+};
+
+export const useStoreModal = create<useStoreModalStore>((set) => ({
+    isOpen: false,
+    onOpen: () => set({ isOpen: true}),
+    onClose: () => set({ isOpen: false}),
+
+}))
 
