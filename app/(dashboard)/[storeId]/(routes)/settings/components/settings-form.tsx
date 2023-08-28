c0e6fbdd2e165f22ec9a1b0f685c6f82a879e3c3
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-08-28 18:45:57 +0200

    errores resolved

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,45 +1,71 @@
-"use cliente";
+"use client";
 
+import React from 'react';
+import * as z from "zod";
 import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
-import { Trash } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
-import * as z from 'zod';
+import { useState } from "react";
+import { Form } from "@/components/ui/form";
 
 
 interface SettingsFormProps {
     initialData: Store;
 }
 
+
 const formSchema = z.object({
     name: z.string().min(1),
-
 });
 
-type SettingFormValues = z.infer<typeof formSchema>
+type SettingFormValues = z.infer<typeof formSchema>;
 
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
+
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm<SettingFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData
+    })
+
+    const onSubmit = async (data: SettingFormValues) => {
+        console.log(data)
+    }
+
     return (
         <>
             <div className="flex items-center justify-between">
                 <Heading 
                     title="Settings"
-                    description="Manage store prefernces"
+                    description="Manage store preferences"
                 />
                 <Button
                     variant="destructive"
                     size="icon"
-                    // onClick={() => {}}
+                    onClick={() => {}}
                 >
                     <Trash className="h-4 w-4"/>
 
                 </Button>
             </div>
             <Separator />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                    <div className="grid grid-cols-3 gap-8">
+
+                    </div>
+                </form>
+            </Form>
         </>
     )
 }
