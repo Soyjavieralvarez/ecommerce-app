23d664935cc656d7c2c63cff6ec10370bae15c91
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-04 19:23:55 +0200

    client page from billboard created

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
+import { Plus } from "lucide-react"
+
+
+const BillboardClient = () => {
+  return (
+    <>
+    <div className="flex items-center justify-between">
+        <Heading
+        title="Billboards (0)"
+        description="Manage billboards for you store" 
+        />
+
+        <Button>
+            <Plus className="mr-2 h-4 w-4" />
+            Add New 
+        </Button>
+    </div>
+    <Separator />
+    </>
+  )
+}
+
+export default BillboardClient
--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,7 +1,12 @@
+import BillboardClient from "./components/client"
+
 const BillboardsPage = () => {
   return (
-    <div>
-        Billboardspage
+    <div className="flex col">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient />
+        </div>
+        
         </div>
   )
 }
