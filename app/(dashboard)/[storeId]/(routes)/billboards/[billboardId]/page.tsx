ee601b3e257318b15518490b26931920b0d7da8c
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-06 18:02:06 +0200

    billboard form created

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,4 +1,5 @@
 import prismadb from "@/lib/prismadb"
+import { BillBoardForm } from "./components/billboard-form"
 
 const BillboardPage = async ({
     params
@@ -13,7 +14,11 @@ const BillboardPage = async ({
     })
 
   return (
-    <div>Existing Billboard: {billboard?.label}</div>
+    <div className="flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+            <BillBoardForm initialData={billboard}/>
+        </div>
+    </div>
   )
 }
 
