c6e01a38f8acee79be74e1ff172d268d08f5f737
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-10 14:37:26 +0200

    data table created

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,12 +3,14 @@
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
-import { Billboard } from "@prisma/client"
 import { Plus } from "lucide-react"
 import { useParams, useRouter  } from "next/navigation"
 
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
+
 interface BillboardClientProps {
-  data: Billboard[]
+  data: BillboardColumn[]
 }
 
 
@@ -32,6 +34,7 @@ export const BillboardClient: React.FC <BillboardClientProps> = ({
         </Button>
     </div>
     <Separator />
+    <DataTable columns={columns} data={data}/>
     </>
   )
 }
