f65436198a0c53782550c35ba7e2f90a9e8192a3
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-11 19:39:45 +0200

    button delete billboards done

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -76,7 +76,7 @@ export const BillBoardForm: React.FC<BillBoardFormProps> = ({
             setLoading(true)
             await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
             router.refresh();
-            router.push("/")
+            router.push(`/${params.storeId}/billboards`)
             toast.success("Billboard deleted.")
         } catch (error) {
             toast.error("Make sure you removed all categories using this billboard first.");
