8a28041c4e559fba1a903d1a59b99eff3275cad2
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-24 13:42:33 +0200

    providers theme dark and light done

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from 'next'
 
 import { ModalProvider } from '@/providers/modal-provider'
 import { ToasterProvider } from '@/providers/toast-provider'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -25,9 +26,11 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
         <ModalProvider />
         <ToasterProvider />
         {children}
+        </ThemeProvider>
         </body>
     </html>
     </ClerkProvider>
